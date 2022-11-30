653477c0a804f7e9ace56ce7144e30c3701d7bc2
Jeong Dong <eejxdong99@gmail.com>
2022-12-01 00:12:26 +0900

    feat: feadUI 일부 구현

--- frontend/sprios_ios/sprios_ios/Controllers/HomeViewController.swift
+++ frontend/sprios_ios/sprios_ios/Controllers/HomeViewController.swift
@@ -9,10 +9,40 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var feedTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        setupTableView()
+    }
+    
+    func setupTableView() {
+        feedTableView.delegate = self
+        feedTableView.dataSource = self
+        
+        feedTableView.rowHeight = UITableView.automaticDimension
+        feedTableView.register(UINib(nibName: "FeedCell", bundle: nil), forCellReuseIdentifier: "FeedCell")
+        
     }
 
+}
+
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedCell", for: indexPath) as! FeedCell
+        
+        return cell
+    }
+    
+    
+}
+
+extension HomeViewController: UITableViewDelegate {
+    
 }
