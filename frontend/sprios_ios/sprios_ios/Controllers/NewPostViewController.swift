04dc98c9d1a04863965a689417e52a5f8f2cafb0
Jeong Dong <eejxdong99@gmail.com>
2022-12-04 17:10:14 +0900

    feat: 게시물 생성 UI 일부 구현

---
+++ frontend/sprios_ios/sprios_ios/Controllers/NewPostViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NewPostViewController.swift
+//  sprios_ios
+//
+//  Created by 이정동 on 2022/12/04.
+//
+
+import UIKit
+
+class NewPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
