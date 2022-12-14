60821841b0ed5b22077a10640713664d4eee38b7
Jeong Dong <eejxdong99@gmail.com>
2022-12-14 23:39:07 +0900

    feat: 프로필 페이지 UI 일부 구현

--- frontend/sprios_ios/sprios_ios/Controllers/MyPageViewController.swift
+++ frontend/sprios_ios/sprios_ios/Controllers/MyPageViewController.swift
@@ -7,23 +7,65 @@
 
 import UIKit
 
-class MyPageViewController: UIViewController {
+struct CVCell1 {
+    static let spacingWidth: CGFloat = 1
+    static let cellColumns: CGFloat = 3
+    private init() {}
+}
 
+class MyPageViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var introduction: UILabel!
+    
+    @IBOutlet weak var editProfileButton: UIButton!
+    
+    // 컬렉션 뷰의 레이아웃을 담당하는 객체
+    let flowLayout = UICollectionViewFlowLayout()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        setupCollectionView()
+    }
+    
+    // 컬렉션 뷰 세팅
+    func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        setupFlowLayout()
+        collectionView.collectionViewLayout = flowLayout
+    }
+
+    func setupFlowLayout() {
+        
+        let collectionCellWidth = (UIScreen.main.bounds.width - CVCell1.spacingWidth * (CVCell1.cellColumns - 1)) / CVCell1.cellColumns
+        
+        flowLayout.scrollDirection = .vertical  // 스크롤 방향
+        flowLayout.itemSize = CGSize(width: collectionCellWidth, height: collectionCellWidth)
+        
+        flowLayout.minimumInteritemSpacing = CVCell1.spacingWidth // 아이템 사이 간격
+        flowLayout.minimumLineSpacing = CVCell1.spacingWidth
+    }
+
+}
+
+// MARK: - CollectionView Delegate, DataSource
+extension MyPageViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
+        cell.photo.image = UIImage(named: "Instagram_logo_2022")
+        return cell
     }
     
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
