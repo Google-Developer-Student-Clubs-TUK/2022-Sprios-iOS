57ea0d73c72d7e24d342486085616777175ab3d4
Jeong Dong <eejxdong99@gmail.com>
2022-12-23 04:15:26 +0900

    refactor: 이미지 URL로 이미지 불러오는 함수 리펙토링

--- frontend/sprios_ios/sprios_ios/Controllers/MyPageViewController.swift
+++ frontend/sprios_ios/sprios_ios/Controllers/MyPageViewController.swift
@@ -45,31 +45,11 @@ class MyPageViewController: UIViewController {
         print(#function)
         profileImage.layer.cornerRadius = profileImage.frame.width / 2
         
-        // MARK: - 수정해야하는 부분 (리펙토링)
-        //var imageUrl = UserDefaults.standard.object(forKey: "imageUrl") as? String
-        
         let imageUrl = user.image?.imgUrl
         
-        guard let urlString = imageUrl, let url = URL(string: urlString) else {
-            return
-        }
-        
-        // 오래걸리는 작업을 동시성 처리 (다른 쓰레드에서 일시킴)
-        DispatchQueue.global().async {
-            // url을 가지고 (이미지)데이터를 가져옴
-            // 동기적으로 처리됨(오래 걸림) -> 2번쓰레드에서 실행하도록 설정
-            guard let data = try? Data(contentsOf: url) else { return }
-            
-            // 오래 걸리는 작업이 일어나는 동안에 url이 바뀔 가능성 제거
-            // 빠른 스크롤 시 1번 url 요청 후 사진을 받아오려는 중에 cell이 재사용되어 2번 url 요청했는데 1번 url 요청 작업이 끝나 2번 사진이 아닌 1번 사진을 보여줄 수도 있기 때문에 url 비교하는 과정
-            guard urlString == url.absoluteString else { return }
-            
-            DispatchQueue.main.async {
-                self.profileImage.image = UIImage(data: data)
-                self.username.text = self.user.name
-                self.introduction.text = self.user.introduce
-            }
-        }
+        self.profileImage.loadImage(imageUrl: imageUrl)
+        self.username.text = self.user.name
+        self.introduction.text = self.user.introduce
 
     }
     
@@ -169,7 +149,7 @@ extension MyPageViewController: UICollectionViewDelegate, UICollectionViewDataSo
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as! PhotoCell
-        cell.imageUrl = postData?.posts[indexPath.row].imageUrls?[0]
+        cell.photo.loadImage(imageUrl: postData?.posts[indexPath.row].imageUrls?[0])
         return cell
     }
     
